57f88ab2ef74128881f8730be61d491555c4f0df
sourabhparmar <sourabh@getvymo.com>
2020-12-15 16:16:48 +0530

    changed name

--- src/components/bio.js
+++ src/components/bio.js
@@ -60,7 +60,7 @@ const Bio = () => {
         Written by <strong>{author.name}</strong> {author.summary}
         {` `}
         <a target="_blank" href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
+          You can follow me on Twitter
         </a>
       </p>
     </div>
